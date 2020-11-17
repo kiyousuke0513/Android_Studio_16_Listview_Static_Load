6f140104eab2ec6604a4a3603fd7e4bfc99a5a6b
kiyousuke0513 <59990082+kiyousuke0513@users.noreply.github.com>
2020-11-17 17:04:41 +0800

    修改靜態實作
    修改靜態實作

--- Listview_Static_Load/app/src/main/java/com/example/listview_static_load/MainActivity.java
+++ Listview_Static_Load/app/src/main/java/com/example/listview_static_load/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,9 +22,8 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listview);
         textView_MSG = findViewById(R.id.textView_MSG);
 
+        textView_MSG.setText("請選擇項目");
         item = getResources().getStringArray(R.array.Item_List_Array);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, item);
-        listView.setAdapter(adapter);
         listView.setOnItemClickListener(listView_OnItemClickListener);
     }
 
@@ -37,8 +35,13 @@ public class MainActivity extends AppCompatActivity {
             textView_MSG.setText("你選擇了: " + item[position]);
             new CountDownTimer(1000, 100)
             {
-                public void onTick(long millisUntilFinished) {textView_MSG.setVisibility(View.VISIBLE);}
-                public void onFinish() {textView_MSG.setVisibility(View.INVISIBLE);}
+                public void onTick(long millisUntilFinished) {
+                    //textView_MSG.setVisibility(View.VISIBLE);
+                }
+                public void onFinish() {
+                    //textView_MSG.setVisibility(View.INVISIBLE);
+                    textView_MSG.setText("請選擇項目");
+                }
             }.start();
         }
     };
